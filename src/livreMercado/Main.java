7d2d990c6fe50743b0a390c2da27a7bd54671520
Augusto-Pedro05 <pedroaugustopr2005@gmail.com>
2024-10-07 21:38:40 -0300

    Renome de arquivos

--- src/livreMercado/Main.java
+++ src/livreMercado/Main.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package ds1;
+package livreMercado;
 
 /**
  *
  * @author pedro
  */
-public class DS1 {
+public class Main {
 
     /**
      * @param args the command line arguments
